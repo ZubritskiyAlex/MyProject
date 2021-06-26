2c5b6a0dd5474cb89dd9807a49076e1f88eea3e9
ZubritskiyAlex <ZubritskiyAlexander@gmail.com>
2021-06-26 15:58:13 +0300

    add actions&reducers

--- myproject-ui/src/App.js
+++ myproject-ui/src/App.js
@@ -1,12 +1,38 @@
+import React,{ Component } from "react";
 import logo from './logo.svg';
 import './App.css';
+import './store';
+import {connect} from "react-redux";
+import {setProducts} from "./action/products";
+import products from "./reducers/products";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Welcome to the Teespring!</h1>
-    </div>
-  );
+class App extends Component{
+  render() {
+      const { products } = this.props.products;
+      const { setBooks } = this.props
+      const newProducts = [{
+          id:0,
+          title: 'Shopping Cart'
+      }];
+      return (
+          <div>
+              <h1>{products[0].title}</h1>
+              <button onClick= {setBooks.bind(this, newProducts)}>SET NEW product</button>
+          </div>
+      );
+  }
 }
 
-export default App;
+const mapState = state => ({
+    ...state
+});
+
+const mapStateToProps = dispatch => ({
+    ...state
+})
+
+const  mapDispatchToProps = dispatch => ({
+    setProducts: products => dispatch(setProducts(products))
+});
+
+export default connect(mapState, mapDispatchToProps)(App);
